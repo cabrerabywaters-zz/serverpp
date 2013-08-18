8ca22661cca9a0aca860bd945e70901b3d80a0bf
Ignacio Ortega <ignacio.ortega@gmail.com>
2013-08-17 22:54:01 -0400

    add demo data. config paperclip media to s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,15 +75,6 @@ Showtime::Application.configure do
   config.action_controller.asset_host = "http://s3.amazonaws.com/#{ENV['FOG_DIRECTORY']}"
   config.action_mailer.default_url_options = { host: ENV['HOST'] }
 
-  config.paperclip_defaults = {
-    storage: :s3,
-    s3_credentials: {
-      bucket: ENV['AWS_BUCKET'],
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
-
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.sendmail_settings = {
     location: '/usr/sbin/sendmail',
