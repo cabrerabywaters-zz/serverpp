6099c474163069f6c3b767221925ffabc8d12b75
Ignacio Ortega <ignacio.ortega@gmail.com>
2013-08-17 22:54:01 -0400

    add demo data. config paperclip media to s3

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130820202928) do
+ActiveRecord::Schema.define(:version => 20130725232353) do
 
   create_table "accounts", :force => true do |t|
     t.integer  "efi_id"
