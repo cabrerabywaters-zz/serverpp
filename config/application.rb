fd2bfa36cf44d66d4abe5cdeb95af7370f186317
Ignacio Ortega <ignacio.ortega@gmail.com>
2013-08-17 22:54:01 -0400

    add demo data. config paperclip media to s3

--- config/application.rb
+++ config/application.rb
@@ -65,5 +65,19 @@ module Showtime
     # Heroku
     config.assets.initialize_on_precompile = false
 
+    # Media
+    if ENV['MEDIA_LOCAL'] == 'false'
+      config.paperclip_defaults =
+        {
+          storage: :s3,
+          s3_credentials: {
+            bucket: ENV['AWS_BUCKET'],
+            access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+            secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+          },
+          #s3_permissions: :private
+        }
+    end
+
   end
 end
