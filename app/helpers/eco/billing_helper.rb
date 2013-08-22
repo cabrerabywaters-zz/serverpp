bbd30849ee34e045f48e9bb607402a008def87e3
CBravo <magno2005@gmail.com>
2013-08-22 09:55:49 -0400

    added first version for billing

---
+++ app/helpers/eco/billing_helper.rb
@@ -0,0 +1,2 @@
+module Eco::BillingHelper
+end
---
+++ spec/helpers/eco/billing_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+# Specs in this file have access to a helper object that includes
+# the Eco::BillingHelper. For example:
+#
+# describe Eco::BillingHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       helper.concat_strings("this","that").should == "this that"
+#     end
+#   end
+# end
+describe Eco::BillingHelper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
