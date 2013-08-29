aed86e8edf08071312b2bfcf89a1d3092717d50e
CBravo <magno2005@gmail.com>
2013-08-29 10:04:18 -0400

    design improvement for ECO Billing

--- app/controllers/eco/billing_controller.rb
+++ app/controllers/eco/billing_controller.rb
@@ -15,12 +15,14 @@ class Eco::BillingController < Eco::EcoApplicationController
 
   def index2
     @data = LazyHighCharts::HighChart.new('column') do |f|
-      f.options[:xAxis] = {categories: ["Marzo", "Abril", "Mayo", "Junio", "Julio"], labels: {enabled: false}} #
+      f.options[:xAxis] = {categories: ["Agosto","Septiembre","Octubre","Noviembre","Diciembre","Enero","Febrero","Marzo", "Abril", "Mayo", "Junio", "Julio"], labels: {enabled: false}}
       f.options[:yAxis] = {labels: {enabled: false}, gridLineWidth: 0, title: {text: ""}}
-      f.series(name: 'Ingresos', data: [150000, 350000, 250000, 210000, 280000])
+      f.series(name: 'Ingresos', data: [50000,70000,100000,120000,150000,200000,180000,280000, 350000, 250000, 210000, 280000])
       f.options[:chart][:defaultSeriesType] = "column"
       f.options[:chart][:height] = 150
+      f.options[:colors] = ["#34495e"]
       f.legend({enabled: false})
+      f.options[:tooltip][:valuePrefix] = "$ "
     end
   end
 
