4abbdac97c8742602dcd02940905bd0c3b2e5f5d
CBravo <magno2005@gmail.com>
2013-09-03 11:38:16 -0400

    added changes on main billing

--- app/controllers/eco/billing_controller.rb
+++ app/controllers/eco/billing_controller.rb
@@ -22,9 +22,9 @@ class Eco::BillingController < Eco::EcoApplicationController
 
   def index2
     @data = LazyHighCharts::HighChart.new('column') do |f|
-      f.options[:xAxis] = {categories: ["Agosto","Septiembre","Octubre","Noviembre","Diciembre","Enero","Febrero","Marzo", "Abril", "Mayo", "Junio", "Julio"], labels: {enabled: false}}
+      f.options[:xAxis] = {categories: ["Agosto","Septiembre","Octubre","Noviembre","Diciembre","Enero","Febrero","Marzo", "Abril", "Mayo", "Junio", "Julio"], labels: { enabled: true, rotation: -45, align: 'right'}}
       f.options[:yAxis] = {labels: {enabled: false}, gridLineWidth: 0, title: {text: ""}}
-      f.series(name: 'Ingresos', data: [50000,70000,100000,120000,150000,200000,180000,280000, 350000, 250000, 210000, 280000])
+      f.series(name: 'Total a Pagar', data: [50000,70000,100000,120000,150000,200000,180000,280000, 350000, 250000, 210000, 280000])
       f.options[:chart][:defaultSeriesType] = "column"
       f.options[:chart][:height] = 150
       f.options[:colors] = ["#34495e"]
