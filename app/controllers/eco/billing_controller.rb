47433e988849fe34d3fbf079579e903ea52e1345
CBravo <magno2005@gmail.com>
2013-09-04 19:18:09 -0400

    added widgets to separate partial on views

--- app/controllers/eco/billing_controller.rb
+++ app/controllers/eco/billing_controller.rb
@@ -3,17 +3,7 @@
 # Public: Controlador encargado manejar la facturacion de una ECO.
 #
 class Eco::BillingController < Eco::EcoApplicationController
-	def index
-		@data = LazyHighCharts::HighChart.new('bar') do |f|
-      f.title({text: 'Ingresos Mensuales'})
-      f.options[:xAxis] = {categories: ["Marzo", "Abril", "Mayo", "Junio", "Julio"]}
-      f.series(name: 'Ingresos', data: [150000, 350000, 250000, 210000, 280000])
-      f.options[:chart][:defaultSeriesType] = "bar"
-      f.legend({enabled: 'false'})
-    end
-  end
-
-  def index2
+  def index
     @data = LazyHighCharts::HighChart.new('column') do |f|
       f.options[:xAxis] = {categories: ["Agosto","Septiembre","Octubre","Noviembre","Diciembre","Enero","Febrero","Marzo", "Abril", "Mayo", "Junio", "Julio"], labels: { enabled: true, rotation: -45, align: 'right'}}
       f.options[:yAxis] = {labels: {enabled: false}, gridLineWidth: 0, title: {text: ""}}
