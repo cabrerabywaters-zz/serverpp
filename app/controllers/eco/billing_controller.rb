a37aedcc391fb37cb705312c609753bbca8b0a93
Ignacio Ortega <ignacio.ortega@gmail.com>
2013-09-05 11:12:16 -0400

    add historical. fix to pay total

--- app/controllers/eco/billing_controller.rb
+++ app/controllers/eco/billing_controller.rb
@@ -6,9 +6,11 @@ class Eco::BillingController < Eco::EcoApplicationController
   def index
     @billing = EcoBillingManager.new current_user_eco.eco
     @data = LazyHighCharts::HighChart.new('column') do |f|
-      f.options[:xAxis] = {categories: ["Agosto","Septiembre","Octubre","Noviembre","Diciembre","Enero","Febrero","Marzo", "Abril", "Mayo", "Junio", "Julio"], labels: { enabled: true, rotation: -45, align: 'right'}}
+      # f.options[:xAxis] = {categories: ["Agosto","Septiembre","Octubre","Noviembre","Diciembre","Enero","Febrero","Marzo", "Abril", "Mayo", "Junio", "Julio"], labels: { enabled: true, rotation: -45, align: 'right'}}
+      f.options[:xAxis] = {categories: get_months, labels: { enabled: true }}
       f.options[:yAxis] = {labels: {enabled: false}, gridLineWidth: 0, title: {text: ""}}
-      f.series(name: 'Total a Pagar', data: [50000,70000,100000,120000,150000,200000,180000,280000, 350000, 250000, 210000, 280000])
+      invoices = @billing.historical.collect(&:to_pay)
+      f.series(name: 'Total a Pagar', data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8000])
       f.options[:chart][:defaultSeriesType] = "column"
       f.options[:chart][:height] = 150
       f.options[:colors] = ["#34495e"]
@@ -21,5 +23,12 @@ class Eco::BillingController < Eco::EcoApplicationController
   def detail
     @billing = EcoBillingManager.new current_user_eco.eco
     @experiences = @billing.transactions_by_experience
+  end
+  
+  private
+  def get_months
+    current_month = Date.current.month
+    months = ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"]
+    12.times.collect { |n| months[(current_month + n - 1) % 12] }
   end
 end
