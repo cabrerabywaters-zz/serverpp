f35eb89e21ee95a45f4ffdac8c1927435a4406ed
Ignacio Ortega <ignacio.ortega@gmail.com>
2013-09-04 22:10:27 -0400

    add demo data for billing

--- spec/factories/experiences.rb
+++ spec/factories/experiences.rb
@@ -41,8 +41,10 @@ FactoryGirl.define do
 
 
     after(:build) do |e|
-      e.industry_experiences << FactoryGirl.build(:industry_experience, experience_id: e.id, percentage: 100.0) unless e.pending?
-      e.valid_images << FactoryGirl.build(:valid_image, experience_id: e.id) if e.eco_id.presence and e.eco.images?
+      e.industry_experiences = [FactoryGirl.build(:industry_experience, experience_id: e.id, percentage: 100.0)] unless e.pending?
+      e.valid_images = [FactoryGirl.build(:valid_image, experience_id: e.id)] if e.eco_id.presence and e.eco.images?
     end
+    
+    initialize_with { Experience.find_or_initialize_by_name(name) }
   end
 end
