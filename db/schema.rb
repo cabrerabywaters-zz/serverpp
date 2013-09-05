699309785a4013294af5c75a1b3518a2361a5780
Ignacio Ortega <ignacio.ortega@gmail.com>
2013-09-05 10:21:03 -0400

    add historical invoices

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130905042554) do
+ActiveRecord::Schema.define(:version => 20130905140612) do
 
   create_table "accounts", :force => true do |t|
     t.integer  "efi_id"
