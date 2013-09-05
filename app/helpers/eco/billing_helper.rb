68ecec5f6f561e790f0b4965dbc8f425e4e82ca8
Ignacio Ortega <ignacio.ortega@gmail.com>
2013-09-05 00:24:27 -0400

    add billing manager

--- app/helpers/eco/billing_helper.rb
+++ app/helpers/eco/billing_helper.rb
@@ -1,2 +1,16 @@
+# coding: utf-8
+
 module Eco::BillingHelper
+  def get_transaction_headers(experience)
+    case experience[:income_type]
+    when "sales"
+      ["Fecha Venta", "Stock Vendido"]
+    when "purchases"
+      ["Fecha Canje", "Código Validación"]
+    when "validations"
+      ["Fecha Validación", "Código Seguridad"]
+    else
+      ["Fecha Validación", "Código Seguridad"]
+    end
+  end
 end
