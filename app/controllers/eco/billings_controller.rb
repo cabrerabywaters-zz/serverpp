c36197b725302ab1dd57841a8f5f7ffa3129b37d
Ignacio Ortega <ignacio.ortega@gmail.com>
2013-09-10 11:16:21 -0300

    fix chart data for previous invoices

--- app/controllers/eco/billings_controller.rb
+++ app/controllers/eco/billings_controller.rb
@@ -9,7 +9,7 @@ class Eco::BillingsController < Eco::EcoApplicationController
       f.options[:xAxis] = {categories: get_months, labels: { enabled: true }}
       f.options[:yAxis] = {labels: {enabled: false}, gridLineWidth: 0, title: {text: ""}}
       invoices = @billing.invoices.collect(&:to_pay)
-      f.series(name: 'Total a Pagar', data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8000])
+      f.series(name: 'Total a Pagar', data: chart_data)
       f.options[:chart][:defaultSeriesType] = "column"
       f.options[:chart][:height] = 150
       f.options[:colors] = ["#34495e"]
@@ -28,5 +28,14 @@ class Eco::BillingsController < Eco::EcoApplicationController
     current_month = Date.current.month
     months = ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"]
     12.times.collect { |n| months[(current_month + n - 1) % 12] }
+  end
+
+  def chart_data
+    data = current_user_eco.eco.invoices.select("to_char(start_at, 'MM') AS month, to_pay").order("start_at desc").limit(12).group_by(&:month)
+    months = get_months
+    months_number = { "Ene" => "01", "Feb" => "02", "Mar" => "03", "Abr" => "04", "May" => "05", "Jun" => "06", "Jul" => "07", "Ago" => "08", "Sep" => "09", "Oct" => "10", "Nov" => "11", "Dic" => "12"}
+    d = months.collect do |m|
+      data[months_number[m]].try(:first).try(:to_pay) || 0
+    end
   end
 end
