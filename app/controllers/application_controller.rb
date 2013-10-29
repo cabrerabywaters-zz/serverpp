c999d5ab69988e809d9d3b9e6d419ecebe032002
Ignacio Ortega <ignacio.ortega@gmail.com>
2013-10-29 12:14:08 -0300

    fix login redirection for user efi

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,12 +15,22 @@ class ApplicationController < ActionController::Base
     if resource_name == :user_eco
       eco_root_path
     elsif resource_name == :user_efi
-      if current_user_efi.present? && current_user_efi.group?(Settings.admin_efi)
+      efi_root_path
+    elsif resource_name == :admin
+      puntos_point_root_path
+    end
+  end
+
+  def after_sign_in_path_for resource
+    if resource.kind_of?(UserEco)
+      eco_root_path
+    elsif resource.kind_of?(UserEfi)
+      if current_user_efi.group?(Settings.admin_efi)
         efi_root_path
       else
         efi_support_path
       end
-    elsif resource_name == :admin
+    elsif resource.kind_of?(Admin)
       puntos_point_root_path
     end
   end
